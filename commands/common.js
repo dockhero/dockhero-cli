971330a11007dc9db2a14a6c1ab327b2c84ccfb9
srgl <sergey.lubyanov@castle.co>
2016-08-02 18:23:28 +0400

    Improve code style, package.json changes

--- commands/common.js
+++ commands/common.js
@@ -7,25 +7,26 @@ let fs = require('fs')
 let mkdirp = require('mkdirp')
 let targz = require('tar.gz')
 
-function getConfig(heroku, app) {
+function getConfig (heroku, app) {
   return heroku.get(`/apps/${app}/config-vars`)
     .then(config => new Promise((resolve, reject) => {
       request({uri: config['DOCKHERO_CONFIG_URL'], headers: {Accept: '*/*'}}, (error, response, body) => {
-
-        if (!error && response.statusCode == 200)
+        if (!error && response.statusCode === 200) {
           resolve(JSON.parse(body))
-        else
+        } else {
           reject(error)
+        }
       })
     }))
 }
 
-function persistCert(config) {
+function persistCert (config) {
   let dockerMachinesFolder = process.env['HOME'] + '/.docker/machine/machines/'
   let machineDir = dockerMachinesFolder + config.name
 
-  if (fs.existsSync(machineDir))
+  if (fs.existsSync(machineDir)) {
     return new Promise(resolve => resolve(machineDir))
+  }
 
   cli.log('getting certs...')
   mkdirp.sync(machineDir)
@@ -34,24 +35,24 @@ function persistCert(config) {
   let stream = read.pipe(write)
 
   return new Promise((resolve, reject) => {
-    stream.on('finish', () => resolve(machineDir));
+    stream.on('finish', () => resolve(machineDir))
     stream.on('error', reject)
   })
 }
 
-function runCommand(command, args = [], env = {}) {
+function runCommand (command, args = [], env = {}) {
   let dockerEnv = Object.assign({}, process.env, env)
-  let child = spawn(command, args, {env: dockerEnv, stdio: [0,1,2]})
+  let child = spawn(command, args, {env: dockerEnv, stdio: [0, 1, 2]})
 
   let promise = new Promise((resolve, reject) => {
-    child.on("close", resolve)
-    child.on("error", reject)
+    child.on('close', resolve)
+    child.on('error', reject)
   })
 
   return promise
 }
 
-function dockerEnv(config) {
+function dockerEnv (config) {
   return persistCert(config).then(certPath => {
     let env = {
       DOCKER_HOST: config.docker_host,
@@ -62,7 +63,6 @@ function dockerEnv(config) {
 
     return env
   })
-
 }
 
 module.exports = {
