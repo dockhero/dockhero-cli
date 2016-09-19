94f1a8c0fcfc8436b97e640d1c990954bea17b0a
srgl <sergey.lubyanov@castle.co>
2016-09-19 17:48:19 +0400

    Refactor waiting of provisioning

--- commands/addon_api.js
+++ commands/addon_api.js
@@ -5,6 +5,9 @@ let utils = require('./utils')
 let ora = require('ora')
 let co = require('co')
 
+let configVarsMissing = `Required config vars are missing, perhaps addon provisioning is still in progress
+Please use heroku addons:open dockhero to check provisioning status`
+
 function* getConfigs(context, heroku) {
   let configVars = yield herokuApi.getConfigVars(context, heroku)
   if (!configVars.DOCKHERO_STAGING_CONFIG_URL) {
@@ -12,7 +15,27 @@ function* getConfigs(context, heroku) {
   }
 
   if (!configVars.DOCKHERO_STAGING_HOST) {
-    yield waitForProvision(configVars.DOCKHERO_STAGING_CONFIG_URL + '/status')
+    let stateUrl = configVars.DOCKHERO_STAGING_CONFIG_URL + '/status'
+    let spinner = null
+    yield waitForProvisioning(getStateProvider(stateUrl), {
+      onStartWaiting: () => {
+        spinner = ora().start()
+      },
+      onProgress: eta => {
+        spinner.text = `Add-on provisioning will finish soon...  ${getMinutesRemaining(eta)}`
+      },
+      onSuccess: () => {
+        if (spinner) {
+          spinner.succeed()
+        }
+      },
+      onFailed: status => {
+        if (spinner) {
+          spinner.fail()
+        }
+        throw new Error('Sorry, add-on provisioning failed. Please remove the add-on and install it once again.')
+      }
+    })
     configVars = yield herokuApi.getConfigVars(context, heroku)
   }
 
@@ -20,33 +43,38 @@ function* getConfigs(context, heroku) {
   return [configVars, dockheroConfig]
 }
 
-function* waitForProvision(statusUrl, spinner = null) {
-  let data = yield cli.got(statusUrl, {json: true}).then(response => response.body);
-
-  if (data.status === 'failed') {
-    if (spinner){
-      spinner.fail();
+function* waitForProvisioning(stateProvider, callbacks) {
+  let initial = true
+  while (true) {
+    let state = yield stateProvider()
+    switch (state.status) {
+      case 'creating':
+        if (initial) {
+          callbacks.onStartWaiting()
+          initial = false
+        }
+        callbacks.onProgress(state.provision_eta)
+        yield utils.delay(5000)
+        break
+      case 'running':
+        callbacks.onSuccess()
+        return true
+      case 'failed':
+        callbacks.onFailed()
+        return false
+      default:
+        throw new Error(`Invalid status: ${state.status}`)
     }
-    throw new Error('Sorry, add-on provisioning failed. Please remove the add-on and install it once again.');
-  }
-
-  if (data.status === 'creating') {
-    spinner = spinner || ora().start();
-    spinner.text = `Add-on provisioning will finish soon...  ${getMinutesRemaining(data.provision_eta)}`;
-    return yield utils.delay(5000).then(() => co(waitForProvision(statusUrl, spinner)));
-  }
-
-  if (data.status === 'running') {
-    if (spinner){
-      spinner.succeed();
-    }
-    return true;
   }
 }
 
+function getStateProvider(stateUrl) {
+  return () => cli.got(stateUrl, {json: true}).then(response => response.body)
+}
+
 function getMinutesRemaining(eta) {
-  let seconds = Math.floor((new Date(eta) - new Date())/1000);
-  return seconds < 0 ? 'almost done...' : [Math.floor(seconds / 60), ':', ('0' + (seconds % 60)).slice(-2)].join();
+  let seconds = Math.floor((new Date(eta) - new Date())/1000)
+  return seconds < 0 ? 'almost done...' : [Math.floor(seconds / 60), ':', ('0' + (seconds % 60)).slice(-2)].join()
 }
 
 function dockerEnv(config) {
