d5db397ea2884810f70f8e10cc9c02de5e477e62
srgl <sergey.lubyanov@castle.co>
2016-09-19 20:10:05 +0400

    Brush up code of dh:generate according to standard rules

--- commands/generate.js
+++ commands/generate.js
@@ -1,69 +1,66 @@
 'use strict'
-let common = require('./common.js')
 let cli = require('heroku-cli-util')
-let co  = require('co')
+let co = require('co')
 let fs = require('fs')
 
 const generatorsUrl = 'https://github.com/dockhero/generators/'
 const rawFilesUrl = 'https://raw.githubusercontent.com/dockhero/generators/master/'
-const readmeUrlPrefix = generatorsUrl + 'blob/master/'
 
 class GithubReader {
-  constructor(generatorName) {
+  constructor (generatorName) {
     this.generatorName = generatorName
   }
 
-  rawFileUrl(fileName) {
+  rawFileUrl (fileName) {
     return rawFilesUrl + this.generatorName + '/' + fileName
   }
 
-  readmeUrl() {
-    return generatorsUrl + "tree/master/" + this.generatorName
+  readmeUrl () {
+    return generatorsUrl + 'tree/master/' + this.generatorName
   }
 
-  rootUrl() {
-    return this.rawFileUrl("") // README is visible in the root of the generator
+  rootUrl () {
+    return this.rawFileUrl('') // README is visible in the root of the generator
   }
 
-  got(filename) {
+  got (filename) {
     return cli.got(this.rawFileUrl(filename))
   }
 }
 
-function* generate (context, heroku) {
+function * generate (context, heroku) {
   const reader = new GithubReader(context.args.name)
   const pkg = yield reader.got('.package.txt')
-  const files = pkg.body.split("\n").map(s => s.trim());
+  const files = pkg.body.split('\n').map(s => s.trim())
 
-  if (!files || files.length == 0) {
-    cli.error(".package.txt not found in ", reader.rootUrl())
-    process.exit(1);
+  if (!files || files.length === 0) {
+    cli.error('.package.txt not found in ', reader.rootUrl())
+    process.exit(1)
   }
 
-  cli.log("Writing files:")
+  cli.log('Writing files:')
 
   const promises = files.map(filename => {
     if (filename) {
       return reader.got(filename).then(response => {
-        cli.log("--> ", filename)
+        cli.log('--> ', filename)
         fs.writeFileSync(filename, response.body)
       })
     }
-  });
+  })
 
-  yield Promise.all(promises);
-  cli.log("Stack generated successfully")
+  yield Promise.all(promises)
+  cli.log('Stack generated successfully')
   cli.open(reader.readmeUrl())
 }
 
-
 module.exports = {
-    topic: 'dh',
-    command: 'generate',
-    description: 'dockhero-generate',
-    help: 'dh:generate <name> - installs the pre-defined example specified by name',
-    needsApp: true,
-    needsAuth: false,
-    args: [{ name: 'name' }],
-    run: cli.command(co.wrap(generate))
+  topic: 'dh',
+  command: 'generate',
+  description: 'dockhero-generate',
+  help: 'dh:generate <name> - installs the pre-defined example specified by name',
+  needsApp: true,
+  needsAuth: false,
+  args: [{ name: 'name' }],
+  run: cli.command(co.wrap(generate))
 }
