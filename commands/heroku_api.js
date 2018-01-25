3de2b80983e6c3c39956731bc434ba3e69c13aef
Vladimir Yartsev <vovayartsev@gmail.com>
2018-01-26 01:41:03 +0400

    Proper use of PREFER_LOCAL_ENV

--- commands/heroku_api.js
+++ commands/heroku_api.js
@@ -1,10 +1,14 @@
 function getAppInfo (context, heroku) {
-  return heroku.get(`/apps/${context.app}`)
+  if (process.env.PREFER_LOCAL_ENV) {
+    return Promise.resolve({name: process.env.HEROKU_APP_NAME, web_url: "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com"})
+  } else {
+    return heroku.get(`/apps/${context.app}`)
+  }
 }
 
 function getConfigVars (context, heroku) {
   if (process.env.PREFER_LOCAL_ENV) {
-    return Promise.resolve({name: process.env.HEROKU_APP_NAME, web_url: "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com"})
+    return Promise.resolve(process.env)
   } else {
     return heroku.get(`/apps/${context.app}/config-vars`)
   }
